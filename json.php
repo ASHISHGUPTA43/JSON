31296ae70018947e445b16f687d915a32562550d
Ashish_Gupta_17 <139530377+ASHISHGUPTA43@users.noreply.github.com>
2025-04-19 20:07:35 +0530

    Json file encoding and decoding

---
+++ json.php
@@ -0,0 +1,10 @@
+<?php
+// $user = ["name" =>"Ash","Age"=> 21,"email"=>"ash@g.com"];
+// // print_r($user);
+// $userJson = json_encode($user);
+// echo $userJson;
+
+// $user = '{"name":"Ash","Age":21,"email":"ash@g.com"}';
+// $userJson = json_decode($user,true);
+// print_r($userJson);
+?>
